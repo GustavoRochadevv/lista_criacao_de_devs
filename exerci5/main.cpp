74bf523b941f361b753f01dcdac74efc5a59a322
GustavoRochadevv <gustavo.rocha.dev@hotmail.com>
2022-07-23 21:19:10 -0300

    iniciando projeto5

---
+++ exerci5/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int numero;
+    cout << "digite um numero ";
+    cin >> numero;
+
+    if (numero > 0 ){
+        cout << "e positivo ";
+    }
+    else{
+        cout << "e negativo";
+    }
+    return 0;
+}
